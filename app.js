093f1b063e07e0d906c3c9d144594f5d29d1e686
Aksha4 <somaniakshaj065@gmail.com>
2026-07-06 19:05:52 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "My first backend project is running!" }));
+});
+
+server.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
